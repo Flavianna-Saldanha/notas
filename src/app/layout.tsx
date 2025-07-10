3639c070e8633b9c5892901853e9906b3fd64a25
Flavianna Saldanha <flaviannasaldanha@gmail.com>
2025-07-10 16:31:34 -0300

    alteração

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="viewport" content="width=device-width, user-scalable=0" />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
